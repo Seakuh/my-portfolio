607f1609949eed3996df70015bb419d363d59574
DizzleReply <D.grimm@reply.de>
2024-08-26 23:56:22 +0200

    add shader to landingpage text

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,27 +1,30 @@
-import React from 'react';
-import './Layout.css';
-import Section from './components/Section/Section';
-import Bubbles from './components/Bubbles/Bubbles';
-import { GithubRepositories } from './components/GithubRepositories/GithubRepositories';
-import PassionSymbols from './components/PassionSymbols/PassionSymbols';
-import TimeLine from './components/TimeLine/TimeLine';
-import BentoGrid from './components/BentoGrid/BentoGrid';
-import { Footer } from './Footer/Footer';
-import LandingText from './LandingText/LandingText';
-import BinauralFrequenciesTable from './BinauralFrequenciesTable/BinauralFrequenciesTable';
+import React from "react";
+import BinauralFrequenciesTable from "./BinauralFrequenciesTable/BinauralFrequenciesTable";
+import BentoGrid from "./components/BentoGrid/BentoGrid";
+import Bubbles from "./components/Bubbles/Bubbles";
+import { GithubRepositories } from "./components/GithubRepositories/GithubRepositories";
+import PassionSymbols from "./components/PassionSymbols/PassionSymbols";
+import Section from "./components/Section/Section";
+import TimeLine from "./components/TimeLine/TimeLine";
+import { Footer } from "./Footer/Footer";
+import "./Layout.css";
+import { Shader } from "./shader/shader";
 
 const Layout: React.FC = () => {
   return (
     <div className="layout">
       <Section content={<Bubbles></Bubbles>} />
-      <Section title="This is me" content={<LandingText></LandingText>} />
-      <Section title={'Blog Posts'} content={<BentoGrid></BentoGrid>} />
+      <Section title="This is me" content={<Shader></Shader>} />
+      <Section title={"Blog Posts"} content={<BentoGrid></BentoGrid>} />
       <Section title="Timeline" content={<TimeLine></TimeLine>} />
       <Section
         title="GithubRepositories"
         content={<GithubRepositories></GithubRepositories>}
       />
-      <Section title='Beats' content={<BinauralFrequenciesTable></BinauralFrequenciesTable>}></Section>
+      <Section
+        title="Beats"
+        content={<BinauralFrequenciesTable></BinauralFrequenciesTable>}
+      ></Section>
       <Section title="Passions" content={<PassionSymbols></PassionSymbols>} />
       <Section content={<Footer></Footer>} />
     </div>
---
+++ src/shader/shader.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useRef } from "react";
+import LandingText from "../LandingText/LandingText";
+import "./shader.css";
+
+export const Shader = () => {
+  // Correctly type the ref as an HTMLIFrameElement
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+
+  // Start the iframe video on load
+  useEffect(() => {
+    const iframe = iframeRef.current;
+    if (iframe) {
+      const playVideo = () => {
+        iframe.contentWindow?.postMessage(
+          '{"event":"command","func":"play","args":""}',
+          "*"
+        );
+      };
+      playVideo();
+    }
+  }, []);
+
+  // Pause and play the iframe on hover
+  const handleMouseEnter = () => {
+    if (iframeRef.current) {
+      iframeRef.current.contentWindow?.postMessage(
+        '{"event":"command","func":"pause","args":""}',
+        "*"
+      );
+    }
+  };
+
+  const handleMouseLeave = () => {
+    if (iframeRef.current) {
+      iframeRef.current.contentWindow?.postMessage(
+        '{"event":"command","func":"play","args":""}',
+        "*"
+      );
+    }
+  };
+
+  return (
+    <div className="shader-container">
+      <div className="text-container">
+        <LandingText />
+      </div>
+      <iframe
+        ref={iframeRef}
+        width="1200"
+        height="1000"
+        src="https://www.shadertoy.com/embed/lcBfRD?gui=false&paused=false&muted=false"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      ></iframe>
+    </div>
+  );
+};
